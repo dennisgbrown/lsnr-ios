13f8e2ef9c63f5026e67f76b76392ffe3db92726
dennisgbrown <dennis.g.brown@gmail.com>
2019-08-18 21:15:16 -0500

    Getting nowhere fast, just checking in the broken stuff I have

--- lsnr/lsnr/ViewController.swift
+++ lsnr/lsnr/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -25,8 +26,61 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         title = "lsnr"
+        
+        statusLabel.text = "Press START to listen"
     }
 
+    //MARK: Actions
+    
+    
+    @IBAction func startListening(_ sender: Any) {
+        
+        statusLabel.text = "LISTENING"
+        
+        print("HELLO!!!!!!!!!!")
+        
+        
+        // Create the capture session.
+        let captureSession = AVCaptureSession()
 
+        print("DERP")
+
+        // Add audio input.
+        do {
+            guard let audioDevice = AVCaptureDevice.default(for: .audio) else {
+                print("it's still garbage")
+                return
+            }
+            let audioDeviceInput = try AVCaptureDeviceInput(device: audioDevice)
+            
+            if captureSession.canAddInput(audioDeviceInput) {
+                captureSession.addInput(audioDeviceInput)
+            }
+            else {
+                print("Could not add audio device input to the session")
+            }
+        }
+        catch {
+            print("Could not create audio device input: \(error)")
+        }
+        
+        print("DERP2")
+        
+
+        print("Start running...")
+        captureSession.startRunning()
+        print("Finished start running...")
+
+        while(true){
+            print("derp ", captureSession.outputs.count)
+        }
+    }
+    
+    
+    @IBAction func stopListening(_ sender: Any) {
+        
+        statusLabel.text = "Press START to listen"
+    }
+    
 }
 
